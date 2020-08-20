7ecd85798c506e86f7c9a8cc042db2b313ecfb47
CherryPerry <CherryPerry@users.noreply.github.com>
2020-08-20 22:32:26 +0100

    Support redesign

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         www.nhk.or.jp video fix
 // @namespace    https://github.com/CherryPerry/userscript-www.nhk.or.jp/
-// @version      1.4
+// @version      1.5
 // @description  Video player fixes for www.nhk.or.jp
 // @author       CherryPerry @ GitHub
 // @match        https://www.nhk.or.jp/*
@@ -13,26 +13,26 @@
 
 (function() {
     'use strict';
-    
+
     // Sample: TODO
 
     let findPlayerContainerV1 = function () {
         return document.querySelector('#app-movie-player > div')
     }
-    
+
     let fixPlayerWidthV1 = function () {
         let root = findPlayerContainerV1()
         root.style.width = '90%'
         root.querySelector('div.clmleft').style.width = '80%'
         root.querySelector('div.clmright').style.width = '20%'
     }
-    
-    // Smaple: https://www2.nhk.or.jp/school/movie/clip.cgi?das_id=D0005330019_00000&p=box
-    
+
+    // Samaple: https://www2.nhk.or.jp/school/movie/clip.cgi?das_id=D0005330019_00000&p=box
+
     let findPlayerContainerV2 = function () {
         return document.querySelector('#movie > div')
     }
-    
+
     let fixPlayerWidthV2 = function () {
         let root = findPlayerContainerV2()
         root.style.width = '90%'
@@ -40,6 +40,21 @@
         root.querySelector('div.clmright').style.width = '20%'
     }
     
+    // Samaple: https://www2.nhk.or.jp/school/movie/bangumi.cgi?das_id=D0005120011_00000 (redesign)
+
+    let findPlayerContainerV3 = function() {
+        return document.querySelector('#mainContents')
+    }
+
+    let fixPlayerWidthV3 = function() {
+        let root = findPlayerContainerV3()
+        root.style.maxWidth = '90%'
+        root.querySelector('section.movie > div').style.maxWidth = '100%'
+        root.querySelector('#moviePlayer').style.width = '80%'
+        root.querySelector('#moviePlayer > div').style.maxWidth = '100%'
+        root.querySelector('#movieChapter').style.width = '20%'
+    }
+
     // Player controls
 
     let findPlayer = function () {
@@ -73,7 +88,7 @@
             }
         })
     }
-    
+
     // Initialization
 
     let waitForElementAndInitialize = function (elementLocator, initializer) {
@@ -97,5 +112,6 @@
 
     waitForElementAndInitialize(findPlayerContainerV1, fixPlayerWidthV1)
     waitForElementAndInitialize(findPlayerContainerV2, fixPlayerWidthV2)
+    waitForElementAndInitialize(findPlayerContainerV3, fixPlayerWidthV3)
     waitForElementAndInitialize(findPlayer, controlPlayerWithKeys)
 })();
